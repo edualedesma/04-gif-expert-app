33b83e863e108667bf88015d33b8eb6cc126ca7b
Eduardo Acuña <eaculed@gmail.com>
2022-12-10 21:43:23 +0000

    Added AddCategory component

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,16 +1,27 @@
 import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
 
-  const [categories, setCategories ] = useState([ 'One Punch', 'Dragon Ball' ]);
+  const [ categories, setCategories ] = useState([ 'One Punch', 'Dragon Ball' ]);
 
-  console.log(categories);
+  const onAddCategory = (newCategory) => {
+
+    if (categories.includes(newCategory)) return;
+    setCategories([ newCategory, ...categories ]);
+    
+  };
 
   return (
     <>
       {/* titulo */}
       <h1>Gif Expert App</h1>
+
       {/* input */}
+      <AddCategory 
+        // setCategories={ setCategories } 
+        onNewCategory={ value => onAddCategory(value) }
+      />
 
       {/* listado de gifs */}
       <ol>
