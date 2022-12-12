c17bfd90cef82418102c282fb1bc45eaafd1b867
Eduardo Acuña <eaculed@gmail.com>
2022-12-12 19:47:43 +0000

    GifItem component. Improvements on GifGrid component

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,17 +1,34 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { GifItem } from './GifItem';
 import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
 
+    const [images, setImages] = useState([]);
+
+    const getImages = async() => {
+        const newImages = await getGifs(category);
+        setImages(newImages);
+    }
+
     useEffect( () => {
-        getGifs(category);
+        getImages();
     }, [])
 
     return (
         <>
-            <div key={ category }>
-                <h3>{ category }</h3>
-                <li>{ category }</li>
+            <h3>{ category }</h3>
+            <div className='card-grid'>
+                { 
+                    images.map( (image) => (
+                        <GifItem 
+                            key={ image.id }
+                            {
+                                ...image
+                            }
+                        />
+                    ))
+                }
             </div>
         </>
     )
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async(category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=N8CoeKpzXWxCwYGr0NqftaoTvHpMb0H6&q=${category}&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=N8CoeKpzXWxCwYGr0NqftaoTvHpMb0H6&q=${category}&limit=10`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
@@ -9,6 +9,5 @@ export const getGifs = async(category) => {
         url: img.images.downsized_medium.url
     }))
 
-    console.log(gifs);
     return gifs;
 }
