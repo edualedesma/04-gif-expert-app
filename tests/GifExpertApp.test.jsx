c1813066f5fc2afbd1d92c1290ebb696b47bc47e
Eduardo Acuña <eaculed@gobiernodecanarias.org>
2022-12-15 10:00:15 +0000

    Testing started on GifExpertApp component

--- tests/GifExpertApp.test.jsx
+++ tests/GifExpertApp.test.jsx
@@ -1,8 +1,25 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { GifExpertApp } from "../src/GifExpertApp";
+
 describe('Testing on <GifExpertApp />', () => {
+
+    test('should match with snapshot', () => {
+
+        const { container } = render(<GifExpertApp />);
+        expect(container).toMatchSnapshot();
+
+    });
 
     test('should first', () => {
 
-        
+        render(<GifExpertApp />);
+
+        const input = screen.getByRole('textbox');
+        fireEvent.input(input, { target: { value: 'One Punch2' } });
+
+        //expect(input.value).toBe('One Punch');
+
+        screen.debug();
 
     });
 
